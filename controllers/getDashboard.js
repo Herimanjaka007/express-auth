e051df71b4910303a159b86d9e58626163ae2bd6
Herimanjaka Razakasoa <geit.herimanjaka@gmail.com>
2024-10-01 14:59:59 +0300

    /dashboard pg vers prisma

--- controllers/getDashboard.js
+++ controllers/getDashboard.js
@@ -1,9 +1,13 @@
-import CustomQuery from "../db/query.js";
+import prisma from "../db/prisma.js";
 
 const getDashboard = async (req, res) => {
     const { user } = req;
     if (user) {
-        const messages = await CustomQuery.getMessagesByAuthor(user);
+        const messages = await prisma.posts.findMany({
+            where: {
+                user_id: user.id,
+            }
+        })
         return res.render("index", { user, messages });
     }
     return res.redirect("/");
